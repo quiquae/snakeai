9d6053715d6d2f19f5a6fc920890d69e1985575e
Jo Warren <jmwarren1997@gmail.com>
2020-07-22 08:56:34 +0100

    toolbar work

--- snake_manual.py
+++ snake_manual.py
@@ -152,17 +152,59 @@ class Game:
 
 class Toolbar:
     toolbarWidth = 0
-    windowWidth = 0
-    windowHeight = 0
-    def __init__(self, width, windoww,windowh):
+    toolbarHeight = 0
+    toolbarX = 0
+
+    def __init__(self, width, windowWidth,windowHeight):
         self.toolbarWidth = width
-        self.windowWidth = windoww
-        self.windowHeight = windowh
-    def draw(self, display, direction, imgs):
-        pygame.draw.rect (display, (255,255,255), ((self.windowWidth,0,self.toolbarWidth, self.windowHeight))) # draws a white rectangle to make the toolbar look different from the board
-        image = pygame.transform.scale(imgs[direction],(int(self.toolbarWidth/2),int(self.toolbarWidth/2))) # take image corresponding to the direction and resclae it to fit toolbar
-        display.blit(image,(int(self.windowWidth+self.toolbarWidth/4),int(self.windowHeight/8))) #blit it so it renders
+        self.toolbarHeight = windowHeight
+        self.toolbarX = windowWidth
+
+    def draw_background(self, display):
+        pygame.draw.rect (display, (0,255,255), (self.toolbarX, 0, self.toolbarWidth, self.toolbarHeight)) # draws a white rectangle to make the toolbar look different from the boar
+    
+    def draw_dpad(self, display, direction): 
+        img = self.images['dpad'][direction]   
+        img = pygame.transform.scale(img, (int(self.toolbarWidth/2), int(self.toolbarWidth/2))) # take image corresponding to the direction and resclae it to fit toolbar
+        display.blit(img,(int(self.toolbarX+self.toolbarWidth/4),int(self.toolbarHeight/8))) #blit it so it renders
+    
+    def draw_food(self, display):
+        pass
+
+    def draw_danger(self, display, state):
+        img_indices = [i + 4*state[i] for i in range(0,4)]
         
+        for idx in img_indices:
+            img = self.images['danger'][idx]
+            img = pygame.transform.scale(img, (int(self.toolbarWidth/2), int(self.toolbarWidth/2))) # take image corresponding to the direction and resclae it to fit toolbar
+            display.blit(img,(int(self.toolbarX+self.toolbarWidth/4),int(self.toolbarHeight/4))) #blit it so it renders
+
+    def draw(self, display, direction):
+        self.draw_background(display)
+        self.draw_dpad(display, direction)
+        #self.draw_danger(display, state)
+    
+    def load_images(self):
+        self.images = {
+            'dpad' : [],
+            'danger' : [],
+            'food' : []
+        }
+
+        self.images['dpad'].append(pygame.image.load("images/dpad/dpad_right.png").convert_alpha())
+        self.images['dpad'].append(pygame.image.load("images/dpad/dpad_left.png").convert_alpha())
+        self.images['dpad'].append(pygame.image.load("images/dpad/dpad_up.png").convert_alpha())
+        self.images['dpad'].append(pygame.image.load("images/dpad/dpad_down.png").convert_alpha())
+
+        self.images['danger'].append(pygame.image.load("images/danger4/left_not.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/right_not.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/up_not.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/down_not.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/left.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/right.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/up.png").convert_alpha())
+        self.images['danger'].append(pygame.image.load("images/danger4/down.png").convert_alpha())
+
 
  
 class App:
@@ -170,7 +212,7 @@ class App:
     windowDimX = 18
     windowWidth = 0
     windowHeight = 0
-    toolbarWidth = 400
+    toolbarWidth = 200
     player = 0
     apple = 0
     toolbar = 0
@@ -194,12 +236,7 @@ class App:
         self._running = True
         self._image_surf = pygame.image.load("images/game_objects/smake.png").convert()
         self._apple_surf = pygame.image.load("images/game_objects/smapple.png").convert()
-        self._direction_images = []
-        self._direction_images.append(pygame.image.load("images/dpad/dpad_right.png").convert())
-        self._direction_images.append(pygame.image.load("images/dpad/dpad_left.png").convert())
-        self._direction_images.append(pygame.image.load("images/dpad/dpad_up.png").convert())
-        self._direction_images.append(pygame.image.load("images/dpad/dpad_down.png").convert())
-        
+        self.toolbar.load_images()        
  
     def on_event(self, event):
         if event.type == QUIT:
@@ -236,7 +273,7 @@ class App:
         self._display_surf.fill((0,0,0))
         self.player.draw(self._display_surf, self._image_surf)
         self.apple.draw(self._display_surf, self._apple_surf)
-        self.toolbar.draw(self._display_surf, self.player.direction, self._direction_images)
+        self.toolbar.draw(self._display_surf, self.player.direction)
         pygame.display.flip()
  
     def on_cleanup(self):
